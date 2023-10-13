7df6191a73a4186fee5a0db89e7551369386a39c
Md. Delower Hossin Dinar <delowerhossin218@gmail.com>
2023-10-13 23:29:11 +0600

    Update G_bowling_figure.c

--- G_bowling_figure.c
+++ G_bowling_figure.c
@@ -1,71 +1,62 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-    int T,i,j;
-    char W;
-    char S[61];
-    scanf("%d",&T);
 float over(int a)
 {
-    int o=a/6;
-    int ball=a%6;
-    float b=o+ball/10.0;
-
+    int o = a / 6;
+    int ball = a % 6;
+    float b = o + ball / 10.0;
     return b;
 }
-    for(i=0;i<T+1;i++)
+int main()
+{
+    int T, i, j;
+    char W;
+    char S[61];
+    scanf("%d", &T);
+
+    for (i = 0; i < T + 1; i++)
     {
         gets(S);
-        int run=0;
-        int wkt=0;
-        int tb=strlen(S);
-        for(j=0;S[j]!='\0';j++)
+        int run = 0;
+        int wkt = 0;
+        int tb = strlen(S);
+        for (j = 0; S[j] != '\0'; j++)
         {
 
-            if(S[j]!=87)
+            if (S[j] != 87)
             {
-                int k=S[j]-48;
-                run=run+k;
+                int k = S[j] - 48;
+                run = run + k;
             }
 
-            if(S[j]==87)
-            wkt=wkt+1;
-
-
-
+            if (S[j] == 87)
+                wkt = wkt + 1;
         }
-        if(i!=0)
+        if (i != 0)
         {
-            float Over=over(j);
+            float Over = over(j);
 
-            if(wkt>1&&run>1&&Over>1)
-                printf("%.1f Overs %d runs %d Wickets.\n",Over,run,wkt);
-            
-            if(wkt>1&&run>1&&Over<=1)
-                printf("%.1f Over %d runs %d Wicket.\n",Over,run,wkt);
+            if (wkt > 1 && run > 1 && Over > 1)
+                printf("%.1f Overs %d runs %d Wickets.\n", Over, run, wkt);
 
-            if(wkt>1&&run<=1&&Over<=1)
-                printf("%.1f Over %d run %d Wickets.\n",Over,run,wkt);
+            if (wkt > 1 && run > 1 && Over <= 1)
+                printf("%.1f Over %d runs %d Wicket.\n", Over, run, wkt);
 
-            if(wkt<=1&&run<=1&&Over<=1)
-                printf("%.1f Over %d run %d Wicket.\n",Over,run,wkt);
+            if (wkt > 1 && run <= 1 && Over <= 1)
+                printf("%.1f Over %d run %d Wickets.\n", Over, run, wkt);
 
-            if(wkt<=1&&run>1&&Over<=1)
-                printf("%.1f Over %d runs %d Wicket.\n",Over,run,wkt);
+            if (wkt <= 1 && run <= 1 && Over <= 1)
+                printf("%.1f Over %d run %d Wicket.\n", Over, run, wkt);
 
-            if(wkt<=1&&run<=1&&Over>1)
-                printf("%.1f Overs %d run %d Wickets.\n",Over,run,wkt);
+            if (wkt <= 1 && run > 1 && Over <= 1)
+                printf("%.1f Over %d runs %d Wicket.\n", Over, run, wkt);
 
+            if (wkt <= 1 && run <= 1 && Over > 1)
+                printf("%.1f Overs %d run %d Wickets.\n", Over, run, wkt);
 
-            if(wkt<=1&&run>1&&Over>1)
-                printf("%.1f Overs %d runs %d Wicket.\n",Over,run,wkt);
-
-
-
+            if (wkt <= 1 && run > 1 && Over > 1)
+                printf("%.1f Overs %d runs %d Wicket.\n", Over, run, wkt);
         }
-
     }
     return 0;
-
 }
